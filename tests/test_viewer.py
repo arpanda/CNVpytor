35a7bf1ec7ac0669e97fc4405debdbde7e226e40
arpanda <arijpanda@gmail.com>
2024-07-18 17:32:24 -0500

    test python version

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def test_module():
     pytor_path = "HepG2_WGS.pytor"
     if Path(pytor_path).exists():
-        app = cnvpytor.Viewer(["HepG2_WGS.pytor"], bin_size=100000)
+        app = cnvpytor.Viewer([pytor_path], params={"bin_size":100000})
         print(app.ls())
     else:
         print(f"Incorrect Path: {pytor_path}")
